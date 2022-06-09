aa357efc6e204759bf8c686b98147a38fe7d6f31
wenzzzel <wenzzzel@live.se>
2022-06-09 21:32:41 +0200

    Registered db context

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
+using GraphQL_demo_api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+builder.Services.AddDbContext<AppDbContext>(
+    opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("AzureSql"))
+);
 
 app.MapGet("/", () => "Hello World!");
 
