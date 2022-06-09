57390c1712402d6f2e5aef819bc6c1e518606600
wenzzzel <wenzzzel@live.se>
2022-06-09 21:50:49 +0200

    Fixed build error

--- Program.cs
+++ Program.cs
@@ -2,12 +2,12 @@ using GraphQL_demo_api.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 builder.Services.AddDbContext<AppDbContext>(
     opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("AzureSql"))
 );
 
+var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
