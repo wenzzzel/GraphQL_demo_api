a4102c1bd3c5a0dc86af9eab65a9c292b10020da
wenzzzel <wenzzzel@live.se>
2022-06-10 13:49:07 +0200

    Created migration for Driver model

--- Migrations/20220610114752_Add-Drivers-to-db.Designer.cs
+++ Migrations/20220610114752_Add-Drivers-to-db.Designer.cs
@@ -3,6 +3,7 @@ using GraphQL_demo_api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GraphQL_demo_api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220610114752_Add-Drivers-to-db")]
+    partial class AddDriverstodb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20220610114752_Add-Drivers-to-db.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GraphQL_demo_api.Migrations
+{
+    public partial class AddDriverstodb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DriverId",
+                table: "Cars",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Drivers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drivers", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_DriverId",
+                table: "Cars",
+                column: "DriverId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cars_Drivers_DriverId",
+                table: "Cars",
+                column: "DriverId",
+                principalTable: "Drivers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cars_Drivers_DriverId",
+                table: "Cars");
+
+            migrationBuilder.DropTable(
+                name: "Drivers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cars_DriverId",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "DriverId",
+                table: "Cars");
+        }
+    }
+}
