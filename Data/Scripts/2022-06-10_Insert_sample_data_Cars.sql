48df914d077f3d4e90d07923dfeaffecba855abf
wenzzzel <wenzzzel@live.se>
2022-06-10 10:14:17 +0200

    Created some sample data in Cars table

---
+++ Data/Scripts/2022-06-10_Insert_sample_data_Cars.sql
@@ -0,0 +1,7 @@
+insert into Cars (Brand, [Name], [Description]) VALUES 
+    ('Volvo', 'V40', 'Small station wagon'),
+    ('Volvo', 'V60', 'Medium sized station wagon'),
+    ('Volvo', 'V90', 'Big station wagon'),
+    ('Volvo', 'S40', 'Small sedan'),
+    ('Volvo', 'S60', 'Medium sized sedan'),
+    ('Volvo', 'S90', 'Big sedan')
