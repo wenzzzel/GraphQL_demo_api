38a7c2a44d5584dbeca31050195658d2e1bdd2fc
wenzzzel <wenzzzel@live.se>
2022-06-10 13:39:23 +0200

    Created Driver model and added relation to Car model

--- Models/Driver.cs
+++ Models/Driver.cs
@@ -2,14 +2,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace GraphQL_demo_api.Models;
 
-public class Car
+public class Driver
 {
     [Key]
     public int Id { get; set; }
     [Required]
-    public string Brand { get; set; }
-    [Required]
     public string Name { get; set; }
-    public string? Description { get; set; }
-    public int DriverId { get; set; }
+    public ICollection<Car> Cars { get; set; } = new List<Car>();
 }
